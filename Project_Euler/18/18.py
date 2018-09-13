2d0f71056767f5444335adbe9639b3d0c10c50d2
kdhfred <unlimiter69@gmail.com>
2018-09-13 14:39:57 +0900

    Update 18.py

--- Project_Euler/18/18.py
+++ Project_Euler/18/18.py
@@ -14,10 +14,7 @@ problem = [[75],
         [63,66,4,68,89,53,67,30,73,16,69,87,40,31],
         [4,62,98,27,23,9,70,98,73,93,38,53,60,4,23]]
 
-answer = 0
-count = 0
 def F(y,x):
-    print problem[y][x]
     result = 0
     if y > 13:
         return problem[y][x]
@@ -29,4 +26,3 @@ def F(y,x):
 
 
 print F(0,0)
-print count
